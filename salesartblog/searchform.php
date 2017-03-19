bc7e41562ea9a596bbf459732681e5bc0faf5ba0
Kirill Kondratenko <kk92@ya.ru>
2017-03-19 14:08:25 +0300

    css edited for mobile

---
+++ salesartblog/searchform.php
@@ -0,0 +1,20 @@
+
+
+<div class="s-search">
+    <p class="s-search__title">Поиск по блогу</p>
+
+    <form class="s-search__form searchform" id="searchform" action="http://salesart.ru/blog/" method="get">
+        <input class="s-search__input" id="s" name="s" type="text" placeholder="Введите запрос" data-required="true">
+    </form>
+</div>
+
+
+
+
+<!--         <form role="search" method="get" id="searchform" class="searchform" action="http://salesart.ru/blog/">
+<div>
+<label class="screen-reader-text" for="s">Найти:</label>
+<input type="text" value="Что" name="s" id="s" />
+<input type="submit" id="searchsubmit" value="Поиск" />
+</div>
+</form> -->
